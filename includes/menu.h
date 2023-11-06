62c962f73ce2bbd3623b1d38fc43457bd343b0be
Debbie Elliott <rigbydebbie@icloud.com>
2023-11-06 21:46:45 +0200

    initial menu class structure

--- includes/menu.h
+++ includes/menu.h
@@ -1,4 +1,19 @@
 #ifndef MENU_H
 # define MENU_H
+#include <SFML/Graphics.hpp>
+#include "utils.h"
+
+namespace game
+{
+	class Menu
+	{
+		public:
+			Menu(sf::RenderWindow *);
+		private:
+		sf::RenderWindow *window;
+
+
+	};
+}
 
 #endif
