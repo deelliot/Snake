17f7f5ca74a4ea4486c28d6327c44397344fda53
Debbie Elliott <rigbydebbie@icloud.com>
2023-11-06 21:45:46 +0200

    added logic for snake to grow

--- src/snake.cpp
+++ src/snake.cpp
@@ -2,20 +2,22 @@
 
 game::Snake::Snake(sf::RenderWindow *window)
 {
-	length = 1;
+	length = 0;
 	colorBody = sf::Color::Blue;
 	colorHead = sf::Color::Yellow;
-	scale = 2;
+	scale = SCALE;
 	this->window = window;
-
-
-	//body.push_back(getRectangleAt(sf::Vector2f(start), colorHead));
 }
 
 void game::Snake::drawSnake()
 {
+	int i = 0;
 	updateSnake();
 	this->window->draw(setBox(head, colorHead));
+	while (i < length)
+	{
+		this->window->draw(setBox(body[i++], colorBody));
+	}
 }
 
 void game::Snake::setDirection(int x, int y)
@@ -26,8 +28,28 @@ void game::Snake::setDirection(int x, int y)
 
 void game::Snake::updateSnake()
 {
+	int i = 0;
+	if (length >= 1)
+	{
+		while (i - 1 < length)
+		{
+			body[i] = body[i + 1];
+			i++;
+		}
+		body[length - 1] = {head.x, head.y};
+	}
 	head.x += direction.x * scale;
 	head.y += direction.y * scale;
+}
+
+void game::Snake::growSnake()
+{
+	sf::Vector2f newSegment;
+
+	length += 1;
+	printf("length: %i", length);
+	newSegment = {head.x, head.y};
+	body.push_back(newSegment);
 }
 
 bool game::Snake::checkCollision(sf::Vector2f objectPosition)
@@ -36,9 +58,16 @@ bool game::Snake::checkCollision(sf::Vector2f objectPosition)
 	sf::RectangleShape object;
 
 	snakeHead.setPosition(head);
-	snakeHead.setSize(sf::Vector2f(BOX_SIZE,BOX_SIZE));
+	snakeHead.setSize(sf::Vector2f(SCALE,SCALE));
 	object.setPosition(objectPosition);
-	object.setSize(sf::Vector2f(BOX_SIZE,BOX_SIZE));
+	object.setSize(sf::Vector2f(SCALE,SCALE));
 
 	return(snakeHead.getGlobalBounds().intersects(object.getGlobalBounds()));
+}
+
+bool game::Snake::checkWindowCollision()
+{
+	if ((head.x < 0 || head.x > WIDTH - 1) || (head.y < 0 || head.y > HEIGHT - 1))
+		return (true);
+	return (false);
 }
