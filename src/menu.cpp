143143b6b49772c70addf48b2147edf922afa82c
Debbie Elliott <rigbydebbie@icloud.com>
2023-11-06 12:16:57 +0200

    removed menu in beginning to keep things simple

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,2 +1,21 @@
 #include <SFML/Window/Keyboard.hpp>
 #include "../includes/menu.h"
+
+// void menu(sf::RenderWindow *window)
+// {
+//     sf::Font gameFont;
+
+//     if(!gameFont.loadFromFile("/Users/debbieelliott/Developer/Snake/font/techno-race-italic-font/TechnoRaceItalic-eZRWe.otf"))
+//         printf("font error");
+//     sf::Text welcomeMessage;
+
+//     welcomeMessage.draw();
+
+//     welcomeMessage.setFont(gameFont);
+//     welcomeMessage.setString("Welcome");
+//     welcomeMessage.setCharacterSize(48);
+//     welcomeMessage.setFillColor(sf::Color::White);
+//     welcomeMessage.setPosition((WIDTH)/2, HEIGHT/2);
+
+//     window->draw(welcomeMessage);
+// }
